31600060579ee5e8404e6077faa310c0126eb676
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2019-08-21 12:05:11 +0200

    Added -R to handle existing stow symlinks.

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo "Any old dotfiles will be moved to $PWD/dotfiles.old."
 for package in $(ls dot); do
     echo "Stowing $package..."
     while [ true ]; do
-        conflicts=$(stow -t $HOME -d dot $package 2>&1 | awk '/\* existing target is/ {print $NF}')
+        conflicts=$(stow -R -t $HOME -d dot $package 2>&1 | awk '/\* existing target is/ {print $NF}')
         for conflict in $conflicts; do
             echo "Detected conflict: $conflict exists."
             mkdir -p dotfiles.old
