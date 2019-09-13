9a08aa22bdb0270f3ec83bf9cca05b109d7ef558
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2019-09-13 16:22:42 +0200

    Kind of first, bad version of v11.

--- dot/zsh/.zsh.d/prompts/v11-script.sh
+++ dot/zsh/.zsh.d/prompts/v11-script.sh
@@ -1,1 +1,13 @@
-echo "$(shuf -i 1000-9999 -n 1) "
+#!/bin/bash
+
+PWD_PART=" %K{black}%F{white}${PWD//"$HOME"/"~"}"
+if [ "$PWD_PART" != "/" ]; then
+    PWD_PART="${PWD_PART//'/'/" \ue0b0 "}"
+fi
+PWD_PART="${PWD_PART/" \ue0b0 "/'/ \ue0b0 '}"
+
+USER_PART=" $USER \ue0b0 $HOSTNAME \ue0b0"
+
+END_PART=" \ue0b0 "
+
+printf "$USER_PART$PWD_PART$END_PART"
